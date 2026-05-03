6b6b07175564c0f85af39aaa592f954451413a23
Linh <22520780@gm.uit.edu.vn>
2026-05-03 14:44:54 +0700

    fix: correct ALLOWED_ORIGINS environment variable in docker-compose and add localhost to CORS configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
   //Enable CORS
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS?.split(',') ?? [
+      'http://localhost:3000',
       'http://localhost:3001',
       'https://next-nest-ecommerce-lovat.vercel.app', // ← thêm domain Vercel
     ],
